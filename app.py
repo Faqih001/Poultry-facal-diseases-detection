b3b3d9ff3b6b883445529b251fd8b297843e8e4a
Amirul Faqih <fakiiahmad001@gmail.com>
2024-03-25 00:27:43 +0300

    Update caching function in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from PIL import Image
 import numpy as np
 
 # Load the model
-@st.cache()
+@st.cache_data(allow_output_mutation=True)
 def load_model():
     model = tf.keras.models.load_model("./model/mobilenetV2/mobilenetv2.h5", compile=False)
     return model
